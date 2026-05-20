ea90495f63feb4fe07eb53f5677898205a7a8db2
iagoarisson28-sudo <iagoarisson28@gmail.com>
2026-05-20 11:42:13 -0300

    Update carro.java

--- carro.java
+++ carro.java
@@ -13,4 +13,15 @@ class Carro {
         System.out.println("Modelo: " + modelo);
         System.out.println("Ano: " + ano);
     }
+    public void exibirInformacoes() (
+//
+//
+
+        String mensagem = "Detalhes do carro:"; // Variável Local (morre
+// ESCOPO DE BLOCO
+//
+        if (modelo |= null) {
+            boolean temodelo = true; // Variável de bloco (só existe de System.out.printIn(mensagem + " Nodelo " + modelo);
+// System.out.println(temModelo); // ERRO! "temModelo" não exist
+        }
 }
